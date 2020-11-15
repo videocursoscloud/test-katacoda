6c06ec06a70b99aa138cc74b108f33ae0cdbda51
Miguel Arranz <miguelarranz>
2020-11-15 01:44:52 +0000

    change

--- vcc-dockerykubernetes-lab1/foreground.sh
+++ vcc-dockerykubernetes-lab1/foreground.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Ejecutando comandos iniciales de mantenimiento, espera un momento"
 
-docker images rm $(docker images|grep REPOSITORY -v|awk '{print $3}') -f > /dev/null
+@docker images rm -f $(docker images|grep REPOSITORY -v|awk '{print $3}')  > /dev/null
 
 echo "Mantenimiento finalizado"
 
